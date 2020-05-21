53b4193c896b3914b15e63799139d5a1645eb0b5
Bruno Fávero <bruno.2956@gmail.com>
2020-05-21 16:34:27 -0300

    validation added to login and signup

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const app = express();
 const mongoDbStore = new MongoDbStoreSession({
     uri: MongoDbUriString,
     collection: 'sessions'
-
 });
 const csrfProtection = csrf();
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,16 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controllers/authCtrl');
+const  validators = require('../middleware/validators');
 
 router.get('/login', authController.getLogin);
 
-router.post('/login', authController.postLogin);
+router.post('/login', validators.postLoginValidator ,authController.postLogin);
 
 router.post('/logout', authController.postLogout);
 
 router.get('/signup', authController.getSignUp);
 
-router.post('/signup', authController.postSignUp);
+router.post('/signup', validators.postSignUpValidator ,
+authController.postSignUp);
 
 router.get('/reset-password', authController.getResetPassword);
 
